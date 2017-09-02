c3e4491ee9e9c6ddb8a6e20200d1bc627ea12118
DmitryFrolov <dmfroloff@gmail.com>
2017-09-02 20:47:37 +0300

    rewrited animated sprite

--- Classes/src/Objects/AnimatedSprite.cpp
+++ Classes/src/Objects/AnimatedSprite.cpp
@@ -19,49 +19,46 @@ void AnimatedSprite::update(float deltaTime)
 {
 	// Update how long the current frame has been displayed
 	frameTime += deltaTime / 1000; // Convert to sec
-	// This check determines if it's time to change to the next frame.
+								   // This check determines if it's time to change to the next frame.
 	if (frameTime > (1 / animFPS))
 		// The number of frames to increment is 
 		// frameTime / (1 / animFPS) == frameTime * animFPS
 		frameNum += frameTime * animFPS;
-			
-	// Check if we've advanced past the last frame, and must wrap.
-	if (frameNum >= animData.frameInfo[animNum].numFrames)
-		frameNum = frameNum % animData.frameInfo[animNum].numFrames;
-	// Update the active image.
-	// (startFrame is relative to all the images, while frameNum is
-	// relative to the first frame of this particular animation).
-	int imageNum = (animData.frameInfo[animNum].startFrame + frameNum) 
-		% animData.frameInfo[animNum].numFrames;
-	Sprite::setTexture((animData.images[imageNum]));
 
+	// Check if we've advanced past the last frame
+	if (frameNum >= currentAnimation.size()) {
+		if (isLooped)
+			frameNum = frameNum % currentAnimation.size();
+		else {
+			animateNext();
+			return;
+		}
+	}
+	Sprite::setTexture((currentAnimation[frameNum]));
 	frameTime = fmod(frameTime, 1 / animFPS);
-
 	Updatable::update(deltaTime);
-}
-
-void AnimatedSprite::initalize(AnimData data, int startingAnimNum)
-{
-	animData = data;
-	changeAnim(startingAnimNum);
-}
-
-void AnimatedSprite::changeAnim(int newAnimNum)
-{
-	animNum = newAnimNum;
-	// The active animation is now at frame 0 and 0.0f time
-	frameNum = 0;
-	//animTime = 0.0f;
-	// Set active image, which is just the starting frame.
-	int imageNum = animData.frameInfo[animNum].startFrame;
-	Sprite::setTexture((animData.images[imageNum]));
-	
 }
 
 void AnimatedSprite::removeChild(NodePtr child)
 {
 	Node::removeChild(child);
 	Updatable::removeChild(child);
+}
+
+void AnimatedSprite::addAnimation(std::string animationName, AnimationFrames frames)
+{
+	assert(!animationName.empty() && !frames.empty());
+	animations.insert(std::make_pair(animationName, frames));
+}
+
+void AnimatedSprite::animate(std::string animationName, std::string nextAnimationName)
+{
+	assert(!animationName.empty() && !nextAnimationName.empty());
+	frameNum = 0;
+	frameTime = 0;
+	isLooped = false;
+	currentAnimation = animations[animationName];
+	nextAnimation = animations[nextAnimationName];
 }
 
 void AnimatedSprite::insertChild(NodePtr child)
@@ -71,6 +68,14 @@ void AnimatedSprite::insertChild(NodePtr child)
 }
 
 
+void AnimatedSprite::animateLooped(std::string loopedAnimationName)
+{
+	currentAnimation = animations[loopedAnimationName];
+	frameNum = 0;
+	frameTime = 0;
+	isLooped = true;
+}
+
 void AnimatedSprite::setAnimFPS(int newFPS)
 {
 	animFPS = newFPS;
@@ -78,4 +83,12 @@ void AnimatedSprite::setAnimFPS(int newFPS)
 int AnimatedSprite::getAnimFPS()
 {
 	return animFPS;
+}
+
+void AnimatedSprite::animateNext()
+{
+	currentAnimation = nextAnimation;
+	isLooped = true;
+	frameNum = 0;
+	frameTime = 0;
 }
